4ae7c401d8428b051f1d29aa2662f8d0f4f7a678
SungHo Park <vladdromero@gmail.com>
2022-05-30 20:14:49 -0400

    Added more item + rendering them on the map

---
+++ 2DWorld/src/object/OBJ_Door.java
@@ -0,0 +1,20 @@
+package object;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class OBJ_Door extends SuperObject {
+
+	public OBJ_Door() {
+
+		name = "Door"; // name of door object
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/door.png")); // door image
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
